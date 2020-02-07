5e71aab7d9bcdd1f2af3fbffd0f5e2041f9c10c1
rossgrams <ross.grams@gmail.com>
2020-02-07 17:09:10 -0500

    InputField: Hook up editFunc and confirmFunc.

--- base widgets/InputField.lua
+++ base widgets/InputField.lua
@@ -6,6 +6,7 @@ local InputField = Button:extend()
 function InputField.setText(self, text, isPlaceholder)
 	self.text = text
 	self.textObj.text = text
+	if self.editFunc then  self:editFunc(text)  end
 	self.theme[self.themeType].setText(self, isPlaceholder)
 end
 
@@ -15,6 +16,11 @@ end
 
 function InputField.backspace(self)
 	self:setText(string.sub(self.text, 1, -2))
+end
+
+function InputField.release(self, dontFire, mx, my, isKeyboard)
+	self.super.release(self, dontFire, mx, my, isKeyboard)
+	if isKeyboard and not dontFire and self.confirmFunc then  self:confirmFunc()  end
 end
 
 function InputField.getFocusNeighbor(self, dir)
